f45778753bc5ec687f90c76eecd8067227e6fbd2
CYTangCY <ChaoYang@lolab-nthu.org>
2020-08-10 01:27:36 +0800

    20/08/10

--- graph.py
+++ graph.py
@@ -6,24 +6,31 @@ from lif_neuron_firingrate_slope import Lif_neuron
 EMAt = [0]
 EMAr = [0]
 _EMA = EMA(EMAt,EMAr)
-_EMA.__init__(EMAt, EMAr)
 _EMA.claculate()
 #print(_EMA.time())
 #print(_EMA.rate())
 
-Lift = [0]
-Lifr = [0]
-_Lif = Lif_neuron(Lift, Lifr)
-_Lif.__init__(Lift, Lifr)
+Lift = []
+Lifr = []
+_v = []
+_Lif = Lif_neuron(Lift, Lifr, _v)
 _Lif.claculate()
-print(Lifr)
+#print(_v)
 
-fig =  plt.figure(figsize =(20,10))
+fig =  plt.figure(figsize =(30,20))
 plt.title('Lif vs EMA')
 plt.xlabel('time')
 plt.ylabel('rate')
 plt.plot(_EMA.time(), _EMA.rate(), 'ro-')
 plt.plot(_Lif.time(), _Lif.rate(), 'go-')
 plt.grid(True)
+
+fig1 = plt.figure(figsize = (20, 10))
+plt.title('Lif_pontential')
+plt.xlabel('time')
+plt.ylabel('pontential')
+plt.plot(_Lif.time(), _Lif.v())
+plt.grid(True)
+
 plt.show()
 
--- lif_neuron_firingrate_slope.py
+++ lif_neuron_firingrate_slope.py
@@ -1,36 +1,47 @@
 import random
 class Lif_neuron:
 
-	def __init__(self, time_Lif, rate_Lif):
+	def __init__(self, time_Lif, rate_Lif, v_Lif):
 		self.time_Lif = time_Lif
 		self.rate_Lif = rate_Lif
-			  		
+		self.v_Lif = v_Lif	  		
 	def claculate(self):
-		g = 2
-		rest = 50
-		threshold = 30
-		reset = 10
+		g = 0.09
+		rest = 30
+		threshold = 50
+		reset = 30
 		noise = 8
 		current_Lif = 50
 		time_count = 0
-		v = rest
 		spike_count = 0
 		Lrate = 0
+		vValue = rest
+		firing = False
 		while Lrate <= 200:
-			v = -g * (v - rest) + current_Lif + random.randint(0, noise) - noise / 2 
-		
-			if v > threshold:
+			vValue = -g * (vValue - rest) + current_Lif + random.randint(0, noise) - noise / 2 
+			#self.v_Lif.append(vValue)
+			#self.time_Lif.append(time_count)
+			#self.rate_Lif.append(Lrate)
+			if vValue >= threshold:
+				firing = True
 				spike_count += 1
-				v = reset
+				vValue = reset
+				self.v_Lif.append(vValue)
+			else:
+				firing = False
+				self.v_Lif.append(vValue)
 			time_count += 1
-			Lrate += spike_count / time_count
+			Lrate = spike_count
 			self.time_Lif.append(time_count)
 			self.rate_Lif.append(Lrate)
-			if len(self.rate_Lif) > 300:
-				break	
+			#if len(self.rate_Lif) > 300:
+				#break	
 	def time(self):
 		return self.time_Lif
 
 	def rate(self):
 		return self.rate_Lif
+
+	def v(self):
+		return self.v_Lif
 	
